f00b764767a73d3d4d8bca6c63b0081cf252104b
AMANJI <amanjiofficial@gmail.com>
2020-04-02 17:27:17 +0530

    Revert "Lottery Game Folder"
    This reverts commit 69fa563e04e75ed12c947ce4f39bef8b0ed1e212.

--- compile.js
+++ compile.js
@@ -2,8 +2,8 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
-const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
-const source = fs.readFileSync(lotteryPath, 'utf8');
+const SourcePath = path.resolve(__dirname,'contracts','Inbox.sol');
+const source = fs.readFileSync(SourcePath, 'utf8');
 
-//console.log(solc.compile(source, 1));
-module.exports = solc.compile(source, 1).contracts[':Lottery'];
+//console.log(solc.compile(source,1));
+module.exports = solc.compile(source,1).contracts[':Inbox'];
